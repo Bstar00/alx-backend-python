c54ebb37a81b622a1031888cf501d29c7b69fe16
Bstar00 <samueloriola212@gmail.com>
2023-08-07 12:54:47 +0000

    4 TASK

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -10,8 +10,8 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ Execute multiple task_wait_random coroutines concurrently and collect results """
     delays: List[float] = [
-    ]       # List to hold the task_wait_random coroutines
-    all_delays: List[float] = []   # List to store the collected delay results
+    ]       """ List to hold the task_wait_random coroutines"""
+    all_delays: List[float] = []
 
     """ Spawn n task_wait_random coroutines with the specified max_delay"""
     for i in range(n):
