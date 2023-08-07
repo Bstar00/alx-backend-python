9f03a73160af588e9b1f501063d9c66f933d59aa
Bstar00 <samueloriola212@gmail.com>
2023-08-07 12:58:54 +0000

    last task

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,18 +8,12 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """ Execute multiple task_wait_random coroutines concurrently and collect results """
-    delays: List[float] = [
-    ]       """ List to hold the task_wait_random coroutines"""
+    """ Tasks """
+    delays: List[float] = []
     all_delays: List[float] = []
-
-    """ Spawn n task_wait_random coroutines with the specified max_delay"""
-    for i in range(n):
+    for _ in range(n):
         delays.append(task_wait_random(max_delay))
-
-    """ Wait for tasks to complete using asyncio.as_completed"""
     for delay in asyncio.as_completed(delays):
         earliest_result = await delay
         all_delays.append(earliest_result)
-
     return all_delays
